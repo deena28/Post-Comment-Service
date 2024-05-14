8bdac5ae6574704c42aad95170cbfc5c9ee7e0c6
Deena P <73109649+deena28@users.noreply.github.com>
2024-05-14 21:57:06 +0530

    Add files via upload

---
+++ PostCommentApplication/src/main/java/com/cdk/backend/PostCommentApplication.java
@@ -0,0 +1,11 @@
+package com.cdk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCommentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PostCommentApplication.class, args);
+	}
+}
