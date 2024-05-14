1b80dae0ed6295ee8eb549b01e7b0eedb5409668
Deena P <73109649+deena28@users.noreply.github.com>
2024-05-14 21:55:10 +0530

    Delete PostCommentApplication directory

--- PostCommentApplication/src/main/java/com/cdk/backend/Repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cdk.backend.Repository;
-
-import com.cdk.backend.Model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
